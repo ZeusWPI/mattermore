1fdc7889cf655455c365b4bc11a04e1bc3191be6
Rien Maertens <rien.maertens@posteo.be>
2019-02-13 21:00:13 +0100

    Server stuff

---
+++ passenger_wsgi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+INTERP = os.path.expanduser("~/env/bin/python3")
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+sys.path.append(os.getcwd())
+
+from app import app as application
