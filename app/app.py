c8fc3572ddbff60ea583080ea4a3e82bce421835
redfast00 <redfast00@gmail.com>
2021-06-17 02:51:28 +0200

    Use user id instead of username for authentication

--- app/app.py
+++ app/app.py
@@ -31,24 +31,38 @@ mm_driver.login()
 from app import models
 from app import cron
 
-def check_regular(username):
-    '''Check if a user has the permissions of a regular user.'''
-    return models.User.query.filter_by(username=username, authorized=True).first() is not None
+
+def get_mattermost_id(username):
+    '''Given a mattermost username, return the user id. Don't call this with stale data'''
+    try:
+        response = mm_driver.users.get_user_by_username(username)
+        return respone['id']
+    except:
+        return None
 
 
-def check_admin(username):
-    '''Check if a user is an admin'''
-    return models.User.query.filter_by(username=username, authorized=True, admin=True).first() is not None
+def query_and_update_username():
+    '''Updates mattermost data if need be. Only use in requests.'''
+    mattermost_user_id = request.values.get('user_id')
+    username = request.values.get('username')
+    user = models.User.query.filter_by(mattermost_id=mattermost_user_id).first()
+    if not user:
+        return None
+    if user.username != username:
+        user.username = username
+        db.session.add(user)
+        db.session.commit()
+    return user
 
 
 def requires_regular(f):
     '''Decorator to require a regular user'''
     @wraps(f)
     def decorated(*args, **kwargs):
-        username = request.values.get('user_name')
-        if not username or not check_regular(username):
+        user = query_and_update_username()
+        if not user or not user.authorized:
             return abort(401)
-        return f(username, *args, **kwargs)
+        return f(user, *args, **kwargs)
     return decorated
 
 
@@ -56,10 +70,10 @@ def requires_admin(f):
     '''Decorator to require an admin user'''
     @wraps(f)
     def decorated(*args, **kwargs):
-        username = request.values.get('user_name')
-        if not username or not check_admin(username):
+        user = query_and_update_username()
+        if not user or not user.authorized or not user.admin:
             return abort(401)
-        return f(username, *args, **kwargs)
+        return f(user, *args, **kwargs)
     return decorated
 
 
@@ -91,22 +105,28 @@ def get_actual_username(username):
 @app.route('/authorize', methods=['POST'])
 @requires_token('authorize')
 @requires_admin
-def authorize(admin_username):
+def authorize(admin_user):
     '''Slash-command to authorize a new user or modify an existing user'''
     tokens = request.values.get('text').strip().split()
-    to_authorize = get_actual_username(tokens[0])
+    if len(tokens) < 1 or len(tokens) > 2:
+        return mattermost_response("Usage: /authorize username [admin]")
+    to_authorize_username = get_actual_username(tokens[0])
+    to_authorize_id = get_mattermost_id(to_authorize_username)
+    if to_authorize_id is None:
+        return mattermost_response("User '{}' does not seem to exist in Mattermost".format(to_authorize_username))
     as_admin = len(tokens) == 2 and tokens[1] == 'admin'
-    user = models.User.query.filter_by(username=to_authorize).first()
+    user = models.User.query.filter_by(mattermost_id=to_authorize_id).first()
     if not user:
-        user = models.User(to_authorize)
+        user = models.User(to_authorize_username)
+        user.mattermost_id = to_authorize_id
     user.authorized = True
     user.admin = as_admin or user.admin
     db.session.add(user)
     db.session.commit()
     if user.admin:
-        return mattermost_response("'{}' is now an admin".format(to_authorize))
+        return mattermost_response("'{}' is now an admin".format(to_authorize_username))
     else:
-        return mattermost_response("'{}' is now a regular user".format(to_authorize))
+        return mattermost_response("'{}' is now a regular user".format(to_authorize_username))
 
 
 @app.route('/revoke', methods=['POST'])
@@ -115,16 +135,19 @@ def authorize(admin_username):
 def revoke(admin_username):
     '''Slash-command to revoke a user'''
     tokens = request.values.get('text').strip().split()
-    to_revoke = get_actual_username(tokens[0])
-    user = models.User.query.filter_by(username=to_revoke).first()
-    if not user:
-        return mattermost_response("Could not find '{}'".format(to_revoke))
+    to_revoke_username = get_actual_username(tokens[0])
+    to_revoke_id = get_mattermost_id(to_revoke_username)
+    if to_revoke_id is None:
+        return mattermost_response("Could not find '{}' in Mattermost".format(to_revoke_username))
+    user = models.User.query.filter_by(mattermost_id=to_revoke_id).first()
+    if user is None:
+        return mattermost_response("Could not find '{}' in our database".format(to_revoke_username))
     if user.admin:
         return mattermost_response("Can't revoke admin user")
     user.authorized = False
     db.session.add(user)
     db.session.commit()
-    return mattermost_response("'{}' revoked".format(to_revoke))
+    return mattermost_response("'{}' revoked".format(to_revoke_username))
 
 
 def slotmachien_request(username, command):
