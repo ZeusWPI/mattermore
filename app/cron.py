1bff2058ed79166c095aa8c822dae3efa3118865
redfast00 <redfast00@gmail.com>
2021-06-18 11:58:27 +0200

    Adapt scraper to new newspage look

--- app/cron.py
+++ app/cron.py
@@ -20,20 +20,19 @@ def get_dict_news():
     soup = BeautifulSoup(r.text, 'html.parser')
     result = []
     for table in soup.find_all('table', ['table-newsoverview']):
-        newsname = table.find_previous_sibling('h2').text
         for row in table.find_all('tr'):
             date = row.find('td', ['date']).find('span').text
             link_element = row.find('a')
             link_id = int(link_element.get('href').split('?id=')[-1])
             link = DICT_NEWS_URL_BASE + link_element.get('href')
             message = link_element.text
-            result.append({'category': newsname, 'id': link_id, 'date': date, 'message': message, 'link': link})
+            result.append({'id': link_id, 'date': date, 'message': message, 'link': link})
     return result
 
 
 
 def post_dict_news(n):
-    message = f'**DICT NIEUWS** *{n["category"]}* op {n["date"]}: [{n["message"]}]({n["link"]})'
+    message = f'**DICT NIEUWS** op {n["date"]}: [{n["message"]}]({n["link"]})'
     print(f"Posting {message}")
     mm_driver.posts.create_post(options={
                 'channel_id': config.sysadmin_channel_id,
@@ -41,7 +40,7 @@ def post_dict_news(n):
     })
 
 
-@scheduler.task('interval', id='dict_news_task', minutes=10)
+@scheduler.task('interval', id='dict_news_task', minutes=5)
 def dict_news_task():
     with app.app_context():
         dict_config = models.KeyValue.query.filter_by(keyname=DICT_NEWS_KEY).first() or models.KeyValue(DICT_NEWS_KEY, "111")
