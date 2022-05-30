0fe40ee1fdbc1fb1ff8e136aecf988a661704387
redfast00 <redfast00@gmail.com>
2022-05-30 21:24:12 +0200

    Disable posting to kelderapi since hoek died

--- app/app.py
+++ app/app.py
@@ -260,12 +260,12 @@ def doorkeeper():
     cmd = data_dict['cmd']
     reason = data_dict['why']
     value = data_dict['val']
-    try:
-        requests.post(config.kelderapi_doorkeeper_url, json=data_dict, headers={'Token': config.kelderapi_doorkeeper_key}, timeout=3)
-    except requests.exceptions.RequestException as e:
-         mattermost_doorkeeper_message(f"Posting {data_dict} to kelderapi failed\n```\n{e.__class__.__name__}: {e}\n```", webhook=config.debug_webhook)
-    except:
-        mattermost_doorkeeper_message(f"Posting {data_dict} to kelderapi failed\n```\n{traceback.format_exc()}\n```", webhook=config.debug_webhook)
+    # try:
+    #     requests.post(config.kelderapi_doorkeeper_url, json=data_dict, headers={'Token': config.kelderapi_doorkeeper_key}, timeout=3)
+    # except requests.exceptions.RequestException as e:
+    #      mattermost_doorkeeper_message(f"Posting {data_dict} to kelderapi failed\n```\n{e.__class__.__name__}: {e}\n```", webhook=config.debug_webhook)
+    # except:
+    #     mattermost_doorkeeper_message(f"Posting {data_dict} to kelderapi failed\n```\n{traceback.format_exc()}\n```", webhook=config.debug_webhook)
     if reason == 'mattermore':
         if cmd == 'status':
             return ''
