d6f97960068a9ad5d78f0abe27d40377ca52c1dd
redfast00 <redfast00@gmail.com>
2024-01-12 20:22:12 +0100

    Implement ratelimiting for error messages

--- app/routes/doorkeeper.py
+++ app/routes/doorkeeper.py
@@ -4,17 +4,44 @@ import hmac
 import requests
 import sys
 import traceback
+from datetime import datetime, timedelta
+
 
 from app.app import DOOR_STATUS
 from app.util import (
     mattermost_doorkeeper_message,
     mark_door_electronically_used,
     in_electronic_action_period,
+    get_persistent_kv
 )
 
 import config
 
+KV_KEY_KELDERAPI_LAST_ERROR_MESSAGE = "kelderapi_last_error_message"
+KV_KEY_KELDERAPI_ERROR_COUNT = "kelderapi_error_count"
+
 doorkeeper_blueprint = Blueprint("doorkeeper", __name__)
+
+
+def post_ratelimited_kelderapi_error(message):
+    '''Post error message to verbose-mattermost channel. Only posts one message every 5 minutes'''
+    last_posted_error_timestring = get_persistent_kv(KV_KEY_KELDERAPI_LAST_ERROR_MESSAGE, "0")
+    last_posted_error_datetime = datetime.fromtimestamp(float(last_posted_error_timestring.value))
+    error_count = get_persistent_kv(KV_KEY_KELDERAPI_ERROR_COUNT, "0")
+    if datetime.now() - last_posted_error_datetime > timedelta(hours=1):
+        last_posted_error_timestring.value = str(datetime.now().timestamp())
+        last_posted_error_timestring.save()
+        if int(error_count.value):
+            message += f"\n and {error_count.value} error message(s) not posted to this channel"
+        mattermost_doorkeeper_message(
+            message,
+            webhook=config.debug_webhook,
+        )
+        error_count.value = "0"
+        error_count.save()
+    else:
+        error_count.value = str(int(error_count.value) + 1)
+        error_count.save()
 
 
 @doorkeeper_blueprint.route("/doorkeeper", methods=["POST"])
@@ -42,14 +69,12 @@ def doorkeeper():
             timeout=3,
         )
     except requests.exceptions.RequestException as e:
-        mattermost_doorkeeper_message(
+        post_ratelimited_kelderapi_error(
             f"Posting `{data_dict}` to kelderapi failed\n```\n{e.__class__.__name__}: {e}\n```",
-            webhook=config.debug_webhook,
         )
     except:
-        mattermost_doorkeeper_message(
+        post_ratelimited_kelderapi_error(
             f"Posting `{data_dict}` to kelderapi failed\n```\n{traceback.format_exc()}\n```",
-            webhook=config.debug_webhook,
         )
     if reason == "mattermore":
         if cmd == "status":
